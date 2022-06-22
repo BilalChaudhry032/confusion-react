1d519785bb83984c25e562ad8d4b7908f0b4dd8b
Bilal Ahmad <bilal.ahmad@suavesolutions.net>
2022-06-22 17:26:55 +0500

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
